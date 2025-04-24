a589ec81129f406e84a70e123aa1463aaec27cbe
DinçerÖğren <dincer.ogr@hotmail.com>
2025-04-24 23:55:05 +0300

    Api call from backend project

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { FaShoppingCart } from "react-icons/fa";
+import ProductViewModel from "./ProductViewModel";
 
  
 
@@ -18,10 +20,27 @@ const ProductCard = ({
     const [selectedViewProduct,setSelectedViewProduct] = useState("");
     const isAvailable=quantity && Number(quantity) > 0; 
     
+    const handleProductView=(product) =>{
+        setSelectedViewProduct(product);
+        setOpenProductViewModel(true);
+        console.log(product)
+    }
+
     return (
 
         <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
-            <div onClick={()=>{}} className="w-full overflow-hidden aspect-[3/2]">
+            <div onClick={()=>{
+                handleProductView({
+                    id: productId,
+                    productName,
+                    image,
+                    description,
+                    quantity,
+                    price,
+                    discount,
+                    specialPrice,
+                })
+            }} className="w-full overflow-hidden aspect-[3/2]">
                 <img className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
                 src={image}
                 alt = {productName}>
@@ -29,12 +48,57 @@ const ProductCard = ({
             </div>
             
             <div className="p-4 ">
-                <h2 onClick={()=>{}}
+                <h2 onClick={()=>{
+                handleProductView({
+                    id: productId,
+                    productName,
+                    image,
+                    description,
+                    quantity,
+                    price,
+                    discount,
+                    specialPrice,
+                    })
+                }}
                     className="text-lg font-semibold mb-2 cursor-pointer">
                         {productName}
                 </h2>
-            </div>
+                <div className="min-h-20 max-h-20">
+                    <p className="text-gray-600 text-sm">{description}</p>
+                </div>
+                <div className="flex justify-between items-center">
+                {specialPrice ? (
+                    <div className="flex flex-col">
+                        <span className="text-gray-400 line-through">
+                            ${Number(price).toFixed(2)}
+                        </span>
+                        <span className="text-xl font-bold text-slate-700">
+                            ${Number(specialPrice).toFixed(2)}
+                        </span>
+                    </div>
+                ): (
+                    <span className="text-xl font-bold text-slate-700">
+                        {"  "}
+                        ${Number(price).toFixed(2)}
+                    </span>
+                )}
 
+                <button
+                    disabled={!isAvailable || btnLoader}
+                    className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
+                            text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
+                    <FaShoppingCart className="mr-2"/>
+                    {isAvailable ? "Add to Cart" : "Stock Out"}
+               
+                </button>
+               </div>
+            </div>
+            <ProductViewModel 
+                open = {openProductViewModel}
+                setOpen = {setOpenProductViewModel}
+                product={selectedViewProduct}
+                isAvailable={isAvailable}/>
+            
         </div>
     )
 
